e993a9c92c17829750bd5a1227e574a1985242c2
Борис Тестов <btestov@belkacar.ru>
2024-01-16 19:22:58 +0500

    init

--- src/main/kotlin/ru/belkacar/pumper/tvdash/rent/service/Configuration.kt
+++ src/main/kotlin/ru/belkacar/pumper/tvdash/rent/service/Configuration.kt
@@ -12,7 +12,7 @@ class Configuration {
     fun restTemplate(builder: RestTemplateBuilder): RestTemplate {
         return builder
             .setConnectTimeout(Duration.ofMillis(3000))
-            .setReadTimeout(Duration.ofMillis(3000))
+            .setReadTimeout(Duration.ofMillis(10000))
             .defaultHeader("x-client", "12345678-90ab-416b-cdef-1234567890ab - One Spell to Rule Them All")
             .defaultHeader("x-api-user", "9d1576db-e881-4477-b017-ad4c9bb2512e")
             .defaultHeader("x-api-key", "f98ccb43-f0d4-4b6d-8e31-b22ccba0d3af")
